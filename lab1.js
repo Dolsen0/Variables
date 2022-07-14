ccdd19cafa1530587b38b175400a5f104f3e9c0a
Derek Olsen <derekolsen@Dereks-MacBook-Pro-2.local>
2022-07-14 11:41:55 -0400

    Variables

---
+++ lab1.js
@@ -0,0 +1,39 @@
+// const favColor = "Blue";
+// console.log(favColor);
+
+// let countBag = 5;
+// let countShoe = 4;
+// console.log(countBag + countShoe);
+
+// const myName = "derek";
+// const sumTotal = (myName[0].toUpperCase() + myName.slice(1) + countBag);
+
+// console.log(sumTotal);
+
+// let truthIsOutThere = true;
+// let falseIsNotGood = false;
+// console.log(truthIsOutThere + falseIsNotGood);
+
+// let nameFirst = "John";
+// let namemiddle = "Candle";
+// let nameLast = "Wick";
+// let nameFull = nameFirst + namemiddle + nameLast
+// console.log(nameFull);
+
+// let firstName = "Derek";
+// let lastName = "Olsen";
+// let fullName = firstName + lastName
+// console.log(fullName);
+
+// let fullNameInCaps = fullName.toUpperCase();
+
+// console.log(fullNameInCaps);
+
+let tax = .7;
+let carWorth = 4500
+let taxAmount = carWorth * tax;
+let totalCar = carWorth + taxAmount;
+
+
+console.log(`My car is worth $${totalCar}`);
+
---
+++ variables.js
@@ -0,0 +1,5 @@
+let age = 35
+const name = "Derek"
+
+console.log(` My age is ${age} and my name is ${name.toLowerCase()} and im not Canadian`);
+
